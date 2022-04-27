683f530fa0cd926189f0f2452f754cb7b40d20ac
AnkitaSen140899 <102920189+AnkitaSen140899@users.noreply.github.com>
2022-04-28 02:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -129,11 +129,6 @@ loader.load('./Star.jpg' , function(texture)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 
 //Light 2
 
